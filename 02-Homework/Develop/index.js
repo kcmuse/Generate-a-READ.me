4752e94b29da1f2e28493dbc35235efb13192e93
Kaleb Muse <kcmuse@Kalebs-MacBook-Air.local>
2021-07-15 10:28:47 -0700

    added base files

---
+++ 02-Homework/Develop/index.js
@@ -0,0 +1,22 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+// TODO: Create an array of questions for user input
+const questions = [{
+    //add your questions here following this format
+    // {
+    //     type: 'checkbox',
+    //     message: 'What languages do you know?',
+    //     choices: [`HTML`, `CSS`, `JavaScript`, `Python`],
+    //     name: 'languages',
+    // },
+}];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
